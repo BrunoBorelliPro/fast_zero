530129a661cd24b13daad78ef670d1bb110cceeb
Bruno Borelli <brunoborellipro@gmail.com>
2024-06-27 16:36:44 -0300

    test updated_at

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from sqlalchemy import select
 
 from fast_zero.models import User
@@ -12,3 +14,20 @@ def test_create_user(session):
     )
 
     assert result.id == 1
+
+
+def test_update_user(session):
+    user = User(username='Bruno', password='senha123', email='bruno@email.com')
+    session.add(user)
+    session.commit()
+
+    sleep(1)
+
+    user = session.scalar(select(User).where(User.id == 1))
+    user.username = 'Bruno Borelli'
+    session.commit()
+
+    result = session.scalar(select(User).where(User.id == 1))
+
+    assert result.username == 'Bruno Borelli'
+    assert result.created_at < result.updated_at
